bf15f5a2dce82d1fd389bef928c72e1e68a746e1
aryan312216 <aryan.sharma2@ucalgary.ca>
2023-02-15 22:53:36 -0700

    ex2.5_data_plotted

--- ex2.5.py
+++ ex2.5.py
@@ -1,13 +1,55 @@
 import json
 import random
+import sys
+import json
+import time
+import matplotlib.pyplot as plt
+import threading
+threading.stack_size(33554432)
+sys.setrecursionlimit(20000)
 
-with open('question2.json', 'r') as f:
-    data = json.load(f)
 
-# shuffle each list
-for key in data:
-    random.shuffle(key)
+def func1(arr, low, high):
+    if low < high:
+        pi = func2(arr, low, high)
+        func1(arr, low, pi-1)
+        func1(arr, pi + 1, high)
 
-# write the shuffled lists to a new JSON file
-with open('ex2.5.json', 'w') as f:
-    json.dump(data,f)
+def func2(array, start, end):
+    p = array[start]
+    low = start + 1
+    high = end
+    while True:
+        while low <= high and array[high] >= p:
+            high = high - 1
+        while low <= high and array[low] <= p:
+            low = low + 1
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+    array[start], array[high] = array[high], array[start]
+  
+    return high
+
+def main():
+    file = open("ex2.5.json", "r")
+    contents = json.load(file)
+    inputSize = []
+    timing = []
+
+# Sort each input and measure the time taken
+    for i, input_array in enumerate(contents):
+        n = len(input_array)
+        inputSize.append(n)
+        start_time = time.time()
+        func1(input_array, 0, n-1)
+        end_time = time.time()
+        timing.append(end_time - start_time)
+    plt.plot(inputSize, timing, 'o-')
+    plt.xlabel("Input Size")
+    plt.ylabel("Sorting Time (s)")
+    plt.title("Quick Sort Timing Results")
+    plt.show()
+
+main()
